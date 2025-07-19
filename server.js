bf4f34db61220eee2ea694cc542099af28ae2169
pateldivyaa <pateldivyad3233@gmail.com>
2025-07-19 22:34:01 +0530

    Add complete backend project

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors({
    origin: [
   'http://localhost:5173',
   'http://127.0.0.1:5173',
-  'https://your-vercel-url.vercel.app'  // Replace with actual Vercel frontend URL
+  'https://blog-frontend-admin-hss7.vercel.app/'  // Replace with actual Vercel frontend URL
 ],
 
     credentials: true,
